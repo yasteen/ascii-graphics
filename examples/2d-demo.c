611eee1671851b2adefe6b4e85a701c2a0f36374
yasteen <justinchan135@gmail.com>
2021-05-23 17:44:15 -0700

    Added example

---
+++ examples/2d-demo.c
@@ -0,0 +1,30 @@
+#include <asciigraphics.h>
+#include <math.h>
+#include <ncurses.h>
+
+int ROW, COL;
+
+int x = 0;
+int r = 20;
+
+void init() {
+	getmaxyx(stdscr, ROW, COL);
+}
+
+// Runs each iteration
+void run() {
+	 fillTriangle(
+	 	(int) (COL / 2 + r * cos(M_PI * x / 180)),
+	 	(int) (ROW + r * sin(M_PI * x / 180)) / 2,
+	 	(int) (COL / 2 + r * cos(M_PI * x / 180 + M_PI * 2 / 3)),
+	 	(int) (ROW + r * sin(M_PI * x / 180 + M_PI * 2 / 3)) / 2,
+	 	(int) (COL / 2 + r * cos(M_PI * x / 180 + M_PI * 4 / 3)),
+	 	(int) (ROW + r * sin(M_PI * x / 180 + M_PI * 4 / 3)) / 2,
+	 	'x');
+	x++;
+}
+
+int main() {
+	startEngine(40, init, run);
+	return 0;
+}
