eba6fdf6b0b9f522013201d39d483f07394a636a
yasteen <justinchan135@gmail.com>
2021-10-30 19:11:35 -0700

    update header file

--- src/include/vecmath.h
+++ src/include/vecmath.h
@@ -35,6 +35,7 @@ extern const mat4 mat4z;
 
 // Functions
 float dotprod(vec v1, vec v2);
+vec3 crossprod(vec3 v1, vec3 v2);
 vec matvecprod(mat, vec v);
 float mat4prod(mat4 m1, mat4 m2);
 vec3 mv3norm(mat4 m, vec3 v);
